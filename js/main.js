df24fd9d98d306466ed2ffe40ec801b3bed9981a
Saswata Gupta <gupta.saswata@gmail.com>
2016-07-04 12:00:23 -0400

    Added player, red rect as placeholder

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-var canvas, ctx, width, height;
+var canvas, ctx, width, height, maxX=40, maxY = 30,
+player = {
+	x: 0,
+	y: 0,
+	draw: function(ctx){
+		ctx.save();
+		ctx.fillStyle = "red";
+		ctx.fillRect(this.x*64, this.y*64, 64, 64);
+		//ctx.stroke();
+		ctx.restore();
+	}
+};
 
 function main(){
 	canvas = document.getElementById('canvas');
@@ -11,6 +22,21 @@ function main(){
 	ctx = canvas.getContext('2d');
 
 	initSprites();
+
+	document.addEventListener('keydown', function(event) {
+	    if (event.which === 87) { //up
+	        player.y = Math.max(0, player.y-1);
+	    }
+	    if (event.which === 83) { //down
+	        player.y = Math.min(maxY, player.y+1);
+	    }
+	    if (event.which === 65) { //left
+	        player.x = Math.max(0, player.x-1);;
+	    }
+	    if (event.which === 68) {
+	        player.x = Math.min(maxX, player.x+1);
+	    }
+	});
 
 	run();
 }
@@ -31,6 +57,7 @@ function update(){
 
 function render(){
 	s_map.draw(ctx, 0, 0, 4);
+	player.draw(ctx);
 }
 
 main();
