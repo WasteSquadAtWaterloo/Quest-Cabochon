9a0f455cb30a85d18dfae9c548c5842c5a794912
Saswata Gupta <gupta.saswata@gmail.com>
2016-07-05 11:06:50 -0400

    Added player and movement

--- js/mapTestMain.js
+++ js/mapTestMain.js
@@ -4,53 +4,64 @@ function preload() {
 
     game.load.tilemap('map', 'assets/Map/sample_map.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles', 'assets/Spritesheet/roguelikeSheet_transparent.png');
+    game.load.spritesheet('player', 'assets/Spritesheet/player1_sheet.png', 48, 48, 96);
 
 }
 
 var map;
 var layer;
 var cursors;
+var player;
 
 function create() {
 
     //  Because we're loading CSV map data we have to specify the tile size here or we can't render it
     map = game.add.tilemap('map');
-
     //  Now add in the tileset
-    map.addTilesetImage('Roguelike','tiles');
-    
+    map.addTilesetImage('Roguelike','tiles');    
     //  Create our layer
-    layer = map.createLayer(0);
-
+    layer = map.createLayer(0);    
     //  Resize the world
-    layer.resizeWorld();
+    layer.resizeWorld();    
+    
+    //fixedToCamera = true;
 
-    //  Allow cursors to scroll around the map
+    player = game.add.sprite(0,0, 'player', 0);
+    player.smoothed = false;
+    player.scale.set(2);
+
+    player.animations.add('down', [0,1,2], 3, true);
+    player.animations.add('left', [3,4,5], 3, true);    
+    player.animations.add('right', [6,7,8], 3, true);
+    player.animations.add('up', [9,10,11], 3, true);
+
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+    game.camera.follow(player);
     cursors = game.input.keyboard.createCursorKeys();
-
-    var help = game.add.text(16, 16, 'Arrows to scroll', { font: '14px Arial', fill: '#ffffff' });
-    help.fixedToCamera = true;
-
 }
 
-function update() {
+function update() {    
 
-    if (cursors.left.isDown)
-    {
-        game.camera.x -= 4;
-    }
-    else if (cursors.right.isDown)
-    {
-        game.camera.x += 4;
-    }
+    player.body.velocity.set(0);
 
-    if (cursors.up.isDown)
-    {
-        game.camera.y -= 4;
+    if (cursors.left.isDown){
+        player.body.velocity.x = -100;
+        player.play('left');
     }
-    else if (cursors.down.isDown)
-    {
-        game.camera.y += 4;
+    else if (cursors.right.isDown){
+        player.body.velocity.x = 100;
+        player.play('right');
+    }
+    else if (cursors.up.isDown){
+        player.body.velocity.y = -100;
+        player.play('up');
+    }
+    else if (cursors.down.isDown){
+        player.body.velocity.y = 100;
+        player.play('down');
+    }
+    else {
+        player.animations.stop();
     }
 
 }
