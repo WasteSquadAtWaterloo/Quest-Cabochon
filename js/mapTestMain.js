9cde2d12831d9812ac7be540d5a43bceef6f4775
Saswata Gupta <gupta.saswata@gmail.com>
2016-07-06 10:31:27 -0400

    Added collsion to some tiles

--- js/mapTestMain.js
+++ js/mapTestMain.js
@@ -5,10 +5,10 @@ function preload() {
     game.load.tilemap('map', 'assets/Map/level_1.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles', 'assets/Spritesheet/roguelikeSheet_transparent.png');
 
-    game.load.spritesheet('{"armor":"none","weapon":"none"}', 'assets/Spritesheet/player/default.png', 64, 65);
-    game.load.spritesheet('{"armor":"leather","weapon":"none"}', 'assets/Spritesheet/player/armor0.png', 64, 65);
-    game.load.spritesheet('{"armor":"plate","weapon":"none"}', 'assets/Spritesheet/player/armor1.png', 64, 65);
-    game.load.spritesheet('{"armor":"gold","weapon":"none"}', 'assets/Spritesheet/player/armor2.png', 64, 65);
+    game.load.spritesheet('{"armor":"none","weapon":"none"}', 'assets/Spritesheet/player/default.png', 64, 64);
+    game.load.spritesheet('{"armor":"leather","weapon":"none"}', 'assets/Spritesheet/player/armor0.png', 64, 64);
+    game.load.spritesheet('{"armor":"plate","weapon":"none"}', 'assets/Spritesheet/player/armor1.png', 64, 64);
+    game.load.spritesheet('{"armor":"gold","weapon":"none"}', 'assets/Spritesheet/player/armor2.png', 64, 64);
 }
 
 var map;
@@ -33,14 +33,12 @@ function create() {
 
     Phaser.Canvas.setSmoothingEnabled(this.game.context, false);
 
-    layer1 = map.createLayer(0); layer1.smoothed = false; layer1.setScale(2.5);
-    layer2 = map.createLayer(1); layer2.smoothed = false; layer2.setScale(2.5);     
-    layer3 = map.createLayer(2); layer3.smoothed = false; layer3.setScale(2.5);    
-    layer4 = map.createLayer(3); layer4.smoothed = false; layer4.setScale(2.5);
-
-    player = game.add.sprite(1600, 1600, JSON.stringify(equip), playerFrames.default.down.walk[0]);
-
-    layer5 = map.createLayer(4); layer5.smoothed = false; layer5.setScale(2.5);
+    layer1 = map.createLayer(0); layer1.smoothed = false; layer1.setScale(3);
+    layer2 = map.createLayer(1); layer2.smoothed = false; layer2.setScale(3);     
+    layer3 = map.createLayer(2); layer3.smoothed = false; layer3.setScale(3);
+    layer4 = map.createLayer(3); layer4.smoothed = false; layer4.setScale(3);
+    player = game.add.sprite(2400, 2400, JSON.stringify(equip), playerFrames.default.down.walk[0]);
+    layer5 = map.createLayer(4); layer5.smoothed = false; layer5.setScale(3);
 
     layer1.resizeWorld();
 
@@ -50,14 +48,19 @@ function create() {
             stand.push(i*57+j+1);
         }
     }
-    stand.push(541, 542, 543, 544, 545, 546, 547, 548, 549, 593, 650);
+
+    for (var i=21; i<25; i++){
+        for (var j=13; j<41; j++){
+            stand.push(i*57+j);
+        }
+    }
+    stand.push(541, 542, 543, 544, 545, 546, 547, 548, 549, 593, 650, 583, 584, 585, 586, 587, 588);
     
     map.setCollisionByExclusion(stand,true,layer1);      
     map.setCollisionByExclusion(stand,true,layer2);  
     map.setCollisionByExclusion(stand,true,layer3);  
     map.setCollisionByExclusion(stand,true,layer4);  
-    map.setCollisionByExclusion(stand,true,layer5);     
-
+    map.setCollisionByExclusion(stand,true,layer5);
 
     player.scale.set(1);
 
@@ -105,7 +108,8 @@ function update() {
     game.physics.arcade.collide(player, layer1);
     game.physics.arcade.collide(player, layer2);
     game.physics.arcade.collide(player, layer3);
-    game.physics.arcade.collide(player, layer4);    
+    game.physics.arcade.collide(player, layer4);
+    game.physics.arcade.collide(player, layer5);     
 
     player.body.velocity.set(0);
 
@@ -133,6 +137,7 @@ function update() {
         dir = playerFrames.default.down.walk[0];
         player_dir = 'down';
     }
+
     else if (game.input.activePointer.leftButton.isDown){ //else if (melee_animation_is_playing){ //melee.isDown
         //melee_animation_is_playing = false;
         if (player_dir === 'down'){
@@ -163,7 +168,7 @@ function update() {
 }
 
 function render() {
-    game.debug.body(player);
+    //game.debug.body(player);
 }
 
 function meleeAnimation() {
