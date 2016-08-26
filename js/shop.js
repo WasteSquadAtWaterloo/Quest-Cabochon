698a0c2b5385ba01ce9c75b9b249dad466c92270
TheCoolLess <nathanjiang@live.com>
2016-08-26 16:33:23 -0400

    made the store take money. Jacked up prices

--- js/shop.js
+++ js/shop.js
@@ -19,17 +19,168 @@ function initShop(){
 
 function make_button (x, y, item){	
 	var btn = game.add.button(x, y, '', function(){
-		var shopTxt = game.add.text(x+shop.cameraOffset.x+15, y+shop.cameraOffset.y+15, "+1" , niceTxtStyle); 
-		shopTxt.fixedToCamera = true; shopTxt.fontSize = 30; 
-		shopTxt.stroke = "black"; shopTxt.strokeThickness = 3; shopTxt.fill = "green";
-		
-	    var tweenTxt = game.add.tween(shopTxt.cameraOffset).to({y: shopTxt.y-75}, 1500, Phaser.Easing.Default, true);	    
-	    
-	    tweenTxt.onComplete.add(function(){
-	        shopTxt.destroy();
-	    });    
 
-		pickUpItems.call(item);
+		if (item == "hp0" && playerGold >= 50){
+			playerGold -= 50;
+			gold.getChildAt(0).setText(playerGold);
+			
+			var shopTxt = game.add.text(x+shop.cameraOffset.x+15, y+shop.cameraOffset.y+15, "+1" , niceTxtStyle); 
+			shopTxt.fixedToCamera = true; shopTxt.fontSize = 30; 
+			shopTxt.stroke = "black"; shopTxt.strokeThickness = 3; shopTxt.fill = "green";
+			
+		    var tweenTxt = game.add.tween(shopTxt.cameraOffset).to({y: shopTxt.y-75}, 1500, Phaser.Easing.Default, true);	    
+		    
+		    tweenTxt.onComplete.add(function(){
+		        shopTxt.destroy();
+		    });    
+
+			pickUpItems.call(item);
+		}
+		else if (item == "hp1" && playerGold >= 100){
+			playerGold -= 100;
+			gold.getChildAt(0).setText(playerGold);
+
+			var shopTxt = game.add.text(x+shop.cameraOffset.x+15, y+shop.cameraOffset.y+15, "+1" , niceTxtStyle); 
+			shopTxt.fixedToCamera = true; shopTxt.fontSize = 30; 
+			shopTxt.stroke = "black"; shopTxt.strokeThickness = 3; shopTxt.fill = "green";
+			
+		    var tweenTxt = game.add.tween(shopTxt.cameraOffset).to({y: shopTxt.y-75}, 1500, Phaser.Easing.Default, true);	    
+		    
+		    tweenTxt.onComplete.add(function(){
+		        shopTxt.destroy();
+		    });    
+
+			pickUpItems.call(item);
+		}
+		else if (item == "hp2" && playerGold >= 200){
+			playerGold -= 200;
+			gold.getChildAt(0).setText(playerGold);
+
+			var shopTxt = game.add.text(x+shop.cameraOffset.x+15, y+shop.cameraOffset.y+15, "+1" , niceTxtStyle); 
+			shopTxt.fixedToCamera = true; shopTxt.fontSize = 30; 
+			shopTxt.stroke = "black"; shopTxt.strokeThickness = 3; shopTxt.fill = "green";
+			
+		    var tweenTxt = game.add.tween(shopTxt.cameraOffset).to({y: shopTxt.y-75}, 1500, Phaser.Easing.Default, true);	    
+		    
+		    tweenTxt.onComplete.add(function(){
+		        shopTxt.destroy();
+		    });    
+
+			pickUpItems.call(item);
+		}
+		else if (item == "mp0" && playerGold >= 50){
+			playerGold -= 50;
+			gold.getChildAt(0).setText(playerGold);
+
+			var shopTxt = game.add.text(x+shop.cameraOffset.x+15, y+shop.cameraOffset.y+15, "+1" , niceTxtStyle); 
+			shopTxt.fixedToCamera = true; shopTxt.fontSize = 30; 
+			shopTxt.stroke = "black"; shopTxt.strokeThickness = 3; shopTxt.fill = "green";
+			
+		    var tweenTxt = game.add.tween(shopTxt.cameraOffset).to({y: shopTxt.y-75}, 1500, Phaser.Easing.Default, true);	    
+		    
+		    tweenTxt.onComplete.add(function(){
+		        shopTxt.destroy();
+		    });    
+
+			pickUpItems.call(item);
+		}
+		else if (item == "mp1" && playerGold >= 100){
+			playerGold -= 100;
+			gold.getChildAt(0).setText(playerGold);
+
+			var shopTxt = game.add.text(x+shop.cameraOffset.x+15, y+shop.cameraOffset.y+15, "+1" , niceTxtStyle); 
+			shopTxt.fixedToCamera = true; shopTxt.fontSize = 30; 
+			shopTxt.stroke = "black"; shopTxt.strokeThickness = 3; shopTxt.fill = "green";
+			
+		    var tweenTxt = game.add.tween(shopTxt.cameraOffset).to({y: shopTxt.y-75}, 1500, Phaser.Easing.Default, true);	    
+		    
+		    tweenTxt.onComplete.add(function(){
+		        shopTxt.destroy();
+		    });    
+
+			pickUpItems.call(item);
+		}
+		else if (item == "mp2" && playerGold >= 200){
+			playerGold -= 200;
+			gold.getChildAt(0).setText(playerGold);
+
+			var shopTxt = game.add.text(x+shop.cameraOffset.x+15, y+shop.cameraOffset.y+15, "+1" , niceTxtStyle); 
+			shopTxt.fixedToCamera = true; shopTxt.fontSize = 30; 
+			shopTxt.stroke = "black"; shopTxt.strokeThickness = 3; shopTxt.fill = "green";
+			
+		    var tweenTxt = game.add.tween(shopTxt.cameraOffset).to({y: shopTxt.y-75}, 1500, Phaser.Easing.Default, true);	    
+		    
+		    tweenTxt.onComplete.add(function(){
+		        shopTxt.destroy();
+		    });    
+
+			pickUpItems.call(item);			
+		}
+		else if (item == "hat0" && playerGold >= 1000){
+			playerGold -= 1000;
+			gold.getChildAt(0).setText(playerGold);
+
+			var shopTxt = game.add.text(x+shop.cameraOffset.x+15, y+shop.cameraOffset.y+15, "+1" , niceTxtStyle); 
+			shopTxt.fixedToCamera = true; shopTxt.fontSize = 30; 
+			shopTxt.stroke = "black"; shopTxt.strokeThickness = 3; shopTxt.fill = "green";
+			
+		    var tweenTxt = game.add.tween(shopTxt.cameraOffset).to({y: shopTxt.y-75}, 1500, Phaser.Easing.Default, true);	    
+		    
+		    tweenTxt.onComplete.add(function(){
+		        shopTxt.destroy();
+		    });    
+
+			pickUpItems.call(item);			
+		}
+		else if (item == "hat1" && playerGold >= 2000){
+			playerGold -= 2000;
+			gold.getChildAt(0).setText(playerGold);
+
+			var shopTxt = game.add.text(x+shop.cameraOffset.x+15, y+shop.cameraOffset.y+15, "+1" , niceTxtStyle); 
+			shopTxt.fixedToCamera = true; shopTxt.fontSize = 30; 
+			shopTxt.stroke = "black"; shopTxt.strokeThickness = 3; shopTxt.fill = "green";
+			
+		    var tweenTxt = game.add.tween(shopTxt.cameraOffset).to({y: shopTxt.y-75}, 1500, Phaser.Easing.Default, true);	    
+		    
+		    tweenTxt.onComplete.add(function(){
+		        shopTxt.destroy();
+		    });    
+
+			pickUpItems.call(item);			
+		}
+		else if (item == "hat2" && playerGold >= 3500){
+			playerGold -= 3500;
+			gold.getChildAt(0).setText(playerGold);
+
+			var shopTxt = game.add.text(x+shop.cameraOffset.x+15, y+shop.cameraOffset.y+15, "+1" , niceTxtStyle); 
+			shopTxt.fixedToCamera = true; shopTxt.fontSize = 30; 
+			shopTxt.stroke = "black"; shopTxt.strokeThickness = 3; shopTxt.fill = "green";
+			
+		    var tweenTxt = game.add.tween(shopTxt.cameraOffset).to({y: shopTxt.y-75}, 1500, Phaser.Easing.Default, true);	    
+		    
+		    tweenTxt.onComplete.add(function(){
+		        shopTxt.destroy();
+		    });    
+
+			pickUpItems.call(item);			
+		}
+		else if (item == "hat3" && playerGold >= 5000){
+			playerGold -= 5000;
+			gold.getChildAt(0).setText(playerGold);
+
+			var shopTxt = game.add.text(x+shop.cameraOffset.x+15, y+shop.cameraOffset.y+15, "+1" , niceTxtStyle); 
+			shopTxt.fixedToCamera = true; shopTxt.fontSize = 30; 
+			shopTxt.stroke = "black"; shopTxt.strokeThickness = 3; shopTxt.fill = "green";
+			
+		    var tweenTxt = game.add.tween(shopTxt.cameraOffset).to({y: shopTxt.y-75}, 1500, Phaser.Easing.Default, true);	    
+		    
+		    tweenTxt.onComplete.add(function(){
+		        shopTxt.destroy();
+		    });    
+
+			pickUpItems.call(item);
+		}
+
 	} ,item);
 
 	btn.width = 60; btn.height = 60;
